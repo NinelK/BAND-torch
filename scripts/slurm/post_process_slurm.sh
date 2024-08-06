4805039e8aebc205a858de00000d2fee30c70607
Nina Kudryashova <nkudryas@ed.ac.uk>
2024-08-06 10:31:59 +0100

    cleanup

--- scripts/slurm/post_process_slurm.sh
+++ scripts/slurm/post_process_slurm.sh
@@ -1,5 +1,5 @@
-model=kl1_gauss_bd
-fac_dim=8
+model=kl1_gauss_bd05
+fac_dim=100
 co_dim=4
 bw=0.1 # band bw #TODO substitute with a binary flag
 
@@ -8,7 +8,7 @@ n_all=207
 n_m1=70
 n_pmd=137
 T=101
-postfix=_d20_causal_ci
+postfix=
 
 for fold in 0 1 2 3 4
 do
@@ -19,8 +19,8 @@ do
     python scripts/ablate_controls.py band-paper-slurm ${model} ${dataset}_cv${fold} band_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
     python scripts/band_performance.py band-paper-slurm ${model} ${dataset}_cv${fold} band_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
 
-    # python scripts/ablate_controls.py band-paper-slurm ${model} ${dataset}_cv${fold} lfads_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-    # python scripts/band_performance.py band-paper-slurm ${model} ${dataset}_cv${fold} lfads_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
+    python scripts/ablate_controls.py band-paper-slurm ${model} ${dataset}_cv${fold} lfads_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
+    python scripts/band_performance.py band-paper-slurm ${model} ${dataset}_cv${fold} lfads_both_${fac_dim}f_${co_dim}c_${model}${postfix} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
 done
 
 # pbt with folds
