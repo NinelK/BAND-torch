4437188190e7693bdef629f1a03a11e43ef360b4
Nina Kudryashova <nkudryas@ed.ac.uk>
2025-02-18 11:01:06 +0000

    add fixed lag behavior decoding

--- scripts/tune_multitask.sh
+++ scripts/tune_multitask.sh
@@ -1,16 +1,25 @@
-model=multitask_noPCR
+model=multitask
 fac_dim=20
 co_dim=4
 bw=0.01 # band bw
 
-dataset=multitasks_20140725
-n=${fac_dim} # number of encoding dimensions (the ones initialized with PCs) = fac_dim
-T=40 # number of timepoints
+dataset=multitasks_tgt_onset_20140725
+#n=${fac_dim} # number of encoding dimensions (the ones initialized with PCs) = fac_dim
+n=75
+T=50 # number of timepoints
 
-python scripts/run_pbt.py ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
-python scripts/ablate_controls.py ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
-python scripts/band_performance.py ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
+PROJECT_STR=pbt-multitasks
 
-python scripts/run_pbt.py ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
-python scripts/band_performance.py ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
+# python scripts/ablate_controls.py ${PROJECT_STR} ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
+# python scripts/band_performance.py ${PROJECT_STR} ${model} ${dataset} lfads_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} 0.
+
+# python scripts/run_pbt.py ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+# python scripts/ablate_controls.py ${PROJECT_STR} ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+# python scripts/band_performance.py ${PROJECT_STR} ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+
+model=multitask_fixed_lag
+
+# python scripts/run_pbt.py ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+# python scripts/ablate_controls.py ${PROJECT_STR} ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
+python scripts/band_performance.py ${PROJECT_STR} ${model} ${dataset} band_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n} ${bw}
