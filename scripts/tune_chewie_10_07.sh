30c99eea9209d292eef2c7f602c6d84a08697e36
Nina Kudryashova <nkudryas@ed.ac.uk>
2025-06-24 14:03:50 +0100

    run suggested 'no controller' experiment

--- scripts/tune_chewie_10_07.sh
+++ scripts/tune_chewie_10_07.sh
@@ -1,7 +1,6 @@
 model=kl1_gauss_bd
 fac_dim=100
-co_dim=4
-bw=0.1 # band bw
+co_dim=0
 
 dataset=chewie_10_07
 n_all=207
@@ -9,7 +8,14 @@ n_m1=70
 n_pmd=137
 T=101
 
+python scripts/run_pbt_defaultHP.py ${model} ${dataset} lfads_both_${fac_dim}f_${co_dim}c_baseline ${T} ${fac_dim} ${co_dim} ${n_all} False
+python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${co_dim}c_${model} ${T} ${fac_dim} ${co_dim} ${n_all} True
 
-# just run a biRNN decoder from predicted rates
-python scripts/band_performance_biRNN.py pbt-band-paper-slurm ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-python scripts/band_performance_biRNN.py pbt-band-paper-slurm ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
+python scripts/run_pbt_defaultHP.py ${model} ${dataset} band_both_${fac_dim}f_${co_dim}c_baseline ${T} ${fac_dim} ${co_dim} ${n_all} False
+python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${co_dim}c_${model} ${T} ${fac_dim} ${co_dim} ${n_all} True
+
+# # just run a biRNN decoder from predicted rates
+# python scripts/band_performance_biRNN.py pbt-band-paper-slurm ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
+# python scripts/band_performance_biRNN.py pbt-band-paper-slurm ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
+
+
