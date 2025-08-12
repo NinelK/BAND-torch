49cc9e354dad25d16fdbaeda3fc0bd1fe69b0b83
Nina Kudryashova <nkudryas@ed.ac.uk>
2025-08-12 15:31:13 +0100

    publication-ready NPG figures

--- scripts/tune_mihili_02_03.sh
+++ scripts/tune_mihili_02_03.sh
@@ -1,8 +1,8 @@
-model=kl1_gauss
-bs=256
+project='pbt-causal-go'
+model=kl1_gauss_bd_d20_causal_ci
 fac_dim=100
 co_dim=4
-bw=0.1 # band bw
+causal=True
 
 dataset=mihili_02_03
 n_all=114
@@ -10,29 +10,29 @@ n_m1=34
 n_pmd=80
 T=92
 
-python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
-python scripts/ablate_controls.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
-python scripts/band_performance.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
+# python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
-python scripts/band_performance.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
-python scripts/ablate_controls.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
-python scripts/band_performance.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
+# python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
-python scripts/band_performance.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-python scripts/ablate_controls.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-python scripts/band_performance.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
+# python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
-python scripts/band_performance.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
 
 dataset=mihili_02_17
 n_all=148
@@ -40,26 +40,86 @@ n_m1=44
 n_pmd=104
 T=85
 
-python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
-python scripts/ablate_controls.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
-python scripts/band_performance.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} 0.
+# python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
-python scripts/band_performance.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_m1} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
-python scripts/ablate_controls.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
-python scripts/band_performance.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} 0.
+# python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
-python scripts/band_performance.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-python scripts/ablate_controls.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
-python scripts/band_performance.py ${model} ${dataset} lfads_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} 0.
+# python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
 
-python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
-python scripts/ablate_controls.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
-python scripts/band_performance.py ${model} ${dataset} band_both_${fac_dim}f_${model}_bs${bs} ${T} ${fac_dim} ${co_dim} ${n_all} ${bw}
+# python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+dataset=mihili_02_18
+n_all=159
+n_m1=38
+n_pmd=121
+T=86
+
+# python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+dataset=mihili_03_07
+n_all=92
+n_m1=26
+n_pmd=66
+T=87
+
+# python scripts/run_pbt.py ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 lfads_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_M1 band_M1_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_m1} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd lfads_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+# python scripts/band_performance.py ${project} ${model} ${dataset}_PMd band_PMd_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_pmd} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+# python scripts/run_pbt.py ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/band_performance.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
