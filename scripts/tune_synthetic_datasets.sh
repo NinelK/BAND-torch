a92019f7beab7c678e0da0e8f94001b5a1b637c7
NinelK <kudryashova.nina@gmail.com>
2026-05-26 11:12:54 +0100

    add correct sh script to run models

---
+++ scripts/tune_synthetic_datasets.sh
@@ -0,0 +1,32 @@
+# project='pbt-pendulum'
+# model=pendulum
+# fac_dim=20
+# co_dim=1
+# causal=True
+
+# dataset=pendulum
+# n_all=200
+# T=100
+
+# python scripts/run_pbt.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+# python scripts/run_pbt.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+# python scripts/ablate_controls.py ${project} ${model} ${dataset} band_both_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+
+project='pbt-lorenz'
+model=lorenz
+fac_dim=20
+co_dim=4
+causal=False
+
+dataset=lorenz
+n_all=200
+T=100
+
+python scripts/run_pbt.py ${project} ${model} ${dataset} lfads_acausal_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/ablate_controls.py ${project} ${model} ${dataset} lfads_acausal_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+
+python scripts/run_pbt.py ${project} ${model} ${dataset} band_acausal_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
+python scripts/ablate_controls.py ${project} ${model} ${dataset} band_acausal_${fac_dim}f_${model} ${T} ${fac_dim} ${co_dim} ${n_all} ${causal}
